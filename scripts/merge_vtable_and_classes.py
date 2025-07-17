2b232b38d02ca2e46852fc97f5a77de0c3217914
Yoav Sternberg <yoav.s@planet-nine.io>
2025-07-17 12:08:12 +0300

    :art: Rewrite merge vtables and classes to be more simple and correct

--- scripts/merge_vtable_and_classes.py
+++ scripts/merge_vtable_and_classes.py
@@ -1,10 +1,12 @@
 import dataclasses
 import json
 import sys
+from collections.abc import Callable
 from dataclasses import dataclass
 from pathlib import Path
 
 UNKNOWN = "???"
+FUNC_PREFIX_UNKNOWN = "sub_"
 
 
 class EnhancedJSONEncoder(json.JSONEncoder):
@@ -34,6 +36,7 @@ class EnhancedJSONEncoder(json.JSONEncoder):
         return {f.name: getattr(obj, f.name) for f in dataclasses.fields(obj)}
 
 
+# region input file json structures
 @dataclass
 class MethodParam:
     type: str
@@ -62,8 +65,8 @@ class InputMethod:
         name = data["name"]
         mangled_name = data["mangled_name"]
         return cls(
-            name="" if name.startswith("sub_") else name,
-            mangled_name="" if mangled_name.startswith("sub_") else mangled_name,
+            name="" if name.startswith(FUNC_PREFIX_UNKNOWN) else name,
+            mangled_name="" if mangled_name.startswith(FUNC_PREFIX_UNKNOWN) else mangled_name,
             return_type=data["return_type"],
             is_pure_virtual=data["is_pure_virtual"],
             is_implemented_by_current_class=data["is_implemented_by_current_class"],
@@ -72,6 +75,10 @@ class InputMethod:
         )
 
 
+# endregion
+
+
+# region output file json structures
 @dataclass
 class MethodWithPrototype:
     prototype_index: int
@@ -105,35 +112,54 @@ class ClassInfo:
             is_abstract=data["is_abstract"],
         )
 
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return isinstance(other, ClassInfo) and self.name == other.name
+
+
+# endregion
+
+type ClassNameToVtable = dict[str, list[MethodWithPrototype]]
+
 
 def main(args):
     if len(args) != 2:
         print("Usage: merge_vtable_and_classes.py classes.json folder_of_methods_json")
         return
-    classes_file_name, folder_of_classes_json = args
+    classes_file_name, folder_of_methods = args
+
+    # Load classes from the provided JSON file
     with open(classes_file_name) as f:
         classes = [ClassInfo.from_dict(cls) for cls in json.load(f)]
+    classes_dict = {c.name: c for c in classes}
 
-    methods: dict[str, list[InputMethod]] = {}  # class_name -> vtable methods
-    for methods_file_name in Path(folder_of_classes_json).glob("*"):
+    # Load input methods from the provided folder
+    input_methods: dict[str, list[InputMethod]] = {}  # class_name -> vtable methods
+    for methods_file_name in Path(folder_of_methods).glob("*"):
         with methods_file_name.open("r") as f:
-            methods.update(
+            input_methods.update(
                 {
                     class_name: [InputMethod.from_dict(m) for m in list_methods]
                     for class_name, list_methods in json.load(f).items()
+                    if class_name in classes_dict
                 }
             )
 
-    new_methods, prototypes = collect_prototypes(methods, classes)
+    # Merge vtables
+    new_methods, prototypes = collect_prototypes(input_methods, classes_dict)
     fix_getters(prototypes)
-    merge(classes, new_methods)
+    write_vtables_to_classes(classes, new_methods)
+
+    # Serialize the results to JSON files
     with open("../src/classes.json", "w") as f:
         json.dump(classes, f, indent=4, cls=EnhancedJSONEncoder)
     with open("../src/prototypes.json", "w") as f:
         json.dump(prototypes, f, indent=4, cls=EnhancedJSONEncoder)
 
 
-def merge(classes: list[ClassInfo], methods: dict[str, list[MethodWithPrototype]]):
+def write_vtables_to_classes(classes: list[ClassInfo], methods: ClassNameToVtable):
     for clazz in classes:
         new_methods = methods.get(clazz.name, None)
         if new_methods:
@@ -148,26 +174,33 @@ def fix_getters(prototypes: list[MethodPrototype]):
 
 
 def collect_prototypes(
-    all_methods: dict[str, list[InputMethod]], classes: list[ClassInfo]
-) -> tuple[dict[str, list[MethodWithPrototype]], list[MethodPrototype]]:
-    classes_dict = {c.name: c for c in classes}  # class_name -> class
+    class_to_input_methods: dict[str, list[InputMethod]], classes: dict[str, ClassInfo]
+) -> tuple[ClassNameToVtable, list[MethodPrototype]]:
     prototypes: list[MethodPrototype] = []
-    new_methods_with_proto: dict[str, list[MethodWithPrototype]] = {}  # class_name -> vtable based on prototypes
-    for class_name, methods in all_methods.items():
-        if class_name not in classes_dict:
-            print("Class not in classes json", class_name)
-            continue
+    class_to_vtable: ClassNameToVtable = {}
 
-        collect_prototypes_for_class(
-            class_name,
-            methods,
+    def handle_class(class_info: ClassInfo):
+        if class_info.name not in class_to_input_methods:
+            print("Class not in input methods json", class_info.name)
+            return
+
+        class_vtable = collect_prototypes_for_class(
+            class_info,
+            class_to_input_methods[class_info.name],
             prototypes,
-            all_methods,
-            classes_dict,
-            new_methods_with_proto,
+            class_to_vtable.get(class_info.parent or "", []),
         )
+        if class_vtable is not None:
+            class_to_vtable[class_info.name] = class_vtable
 
-    # Fix pure virtual methods
+    dfs_classes(classes, handle_class)
+
+    fix_pure_virtual_methods(prototypes)
+    return class_to_vtable, prototypes
+
+
+def fix_pure_virtual_methods(prototypes: list[MethodPrototype]):
+    """If some methods left without a name (came from pure virtual methods), add a fake name and type."""
     for prototype in prototypes:
         if prototype.name == "":
             prototype.name = f"vmethod{prototype.vtable_index}"
@@ -175,48 +208,43 @@ def collect_prototypes(
             prototype.return_type = UNKNOWN
             prototype.parameters = [MethodParam(type=UNKNOWN)]
 
-    return new_methods_with_proto, prototypes
+
+def dfs_classes(classes: dict[str, ClassInfo], callback: Callable[[ClassInfo], None]):
+    """
+    Depth-first search through the class hierarchy, calling `callback` for each class.
+    Callback for class is only called after all its parents were called.
+    """
+
+    visited: set[ClassInfo] = set()
+
+    def dfs(cls: ClassInfo):
+        if cls in visited:
+            return
+        visited.add(cls)
+
+        if cls.parent:
+            dfs(classes[cls.parent])
+        callback(cls)
+
+    for clazz in classes.values():
+        dfs(clazz)
 
 
 def collect_prototypes_for_class(
-    class_name: str,
+    class_info: ClassInfo,
     methods: list[InputMethod],
     prototypes: list[MethodPrototype],
-    all_methods: dict[str, list[InputMethod]],
-    classes_dict: dict[str, ClassInfo],
-    new_methods_with_proto: dict[str, list[MethodWithPrototype]],
-):
-    # If already run, don't run again.
-    if class_name in new_methods_with_proto:
-        return
+    parent_vtable: list[MethodWithPrototype],
+) -> list[MethodWithPrototype] | None:
+    class_name = class_info.name
+    my_methods: list[MethodWithPrototype] = []
 
-    # For each parent, collect its prototypes.
-    my_class = classes_dict[class_name]
+    if len(parent_vtable) > len(methods):
+        print(f"Not enough methods for {class_name}. Expected: {len(parent_vtable)} has: {len(methods)}")
+        return None
 
-    clz: ClassInfo | None = my_class
-    while (clz := classes_dict.get(clz.parent or "")) is not None:
-        if clz.name in all_methods:
-            collect_prototypes_for_class(
-                clz.name,
-                all_methods[clz.name],
-                prototypes,
-                all_methods,
-                classes_dict,
-                new_methods_with_proto,
-            )
-
-    parent = my_class.parent
-    parent_proto_methods = new_methods_with_proto.get(parent or "", [])
-
-    my_methods = []
-    if len(parent_proto_methods) > len(methods):
-        print(f"Not enough methods for {class_name}. Expected: {len(parent_proto_methods)} has: {len(methods)}")
-        return
-
-    for i in range(len(parent_proto_methods)):
-        parent_method = parent_proto_methods[i]
-        input_method = methods[i]
-
+    # Collect and enrich parent methods' prototypes
+    for parent_method, input_method in zip(parent_vtable, methods, strict=False):
         my_methods.append(
             MethodWithPrototype(
                 parent_method.prototype_index,
@@ -228,17 +256,28 @@ def collect_prototypes_for_class(
         enrich_prototype(class_name, prototypes[parent_method.prototype_index], input_method)
 
     # Add new methods' prototypes
-    for i in range(len(parent_proto_methods), len(methods)):
+    for i in range(len(parent_vtable), len(methods)):
         input_method = methods[i]
-        prototype = MethodPrototype(
-            name="" if input_method.is_pure_virtual else input_method.name,
-            mangled_name="" if input_method.is_pure_virtual else input_method.mangled_name,
-            return_type=UNKNOWN if input_method.is_pure_virtual else input_method.return_type,
-            parameters=input_method.parameters,
-            vtable_index=input_method.vtable_index,
-            declaring_class=class_name,
-            proto_index=len(prototypes),
-        )
+        if input_method.is_pure_virtual:
+            prototype = MethodPrototype(
+                name="",
+                mangled_name="",
+                return_type=UNKNOWN,
+                parameters=[MethodParam(type=UNKNOWN)],
+                vtable_index=input_method.vtable_index,
+                declaring_class=class_name,
+                proto_index=len(prototypes),
+            )
+        else:
+            prototype = MethodPrototype(
+                name=input_method.name,
+                mangled_name=input_method.mangled_name,
+                return_type=input_method.return_type,
+                parameters=input_method.parameters,
+                vtable_index=input_method.vtable_index,
+                declaring_class=class_name,
+                proto_index=len(prototypes),
+            )
         prototypes.append(prototype)
         my_methods.append(
             MethodWithPrototype(
@@ -248,33 +287,47 @@ def collect_prototypes_for_class(
             )
         )
 
-    new_methods_with_proto[class_name] = my_methods
+    return my_methods
 
 
 def enrich_prototype(class_name: str, prototype: MethodPrototype, input_method: InputMethod):
+    # Pure virtual method does not have any details we can use
+    if input_method.is_pure_virtual:
+        return
+
+    # If we don't know the return type, we can use the input method's return type.
     if prototype.return_type == UNKNOWN:
         prototype.return_type = input_method.return_type
 
-    if input_method.is_pure_virtual:
-        return
-    elif (len(prototype.parameters) == 1 and prototype.parameters[0].type == UNKNOWN) or prototype.name == "":
-        prototype.parameters = input_method.parameters
+    # If we were defined by pure virtual method, we can use the override method's name and mangled name.
+    if not prototype.name:
         prototype.name = prototype.name or input_method.name
         prototype.mangled_name = prototype.mangled_name or input_method.mangled_name
+    # Check if the prototype name matches the input method name.
     elif (
         prototype.name != input_method.name
         and not prototype.name.startswith("~")
-        and not input_method.name.startswith("sub_")
+        and not input_method.name.startswith(FUNC_PREFIX_UNKNOWN)
     ):
-        print("Name mismatch:", class_name, prototype, input_method)
+        print(f"[Error] Name mismatch on {class_name}: \n\t{prototype}\n\t{input_method}")
         return
-    else:
+
+    enrich_parameters(class_name, prototype, input_method)
+
+
+def enrich_parameters(class_name: str, prototype: MethodPrototype, input_method: InputMethod):
+    """Enrich the parameters of the prototype with the input method's parameters."""
+
+    # If we have unknown parameters, we can use the input method's parameters to fill them.
+    if _has_unknown_parameters(prototype.parameters):
+        prototype.parameters = input_method.parameters
+    # Otherwise, try to enrich the parameters themselves.
+    if not _has_unknown_parameters(input_method.parameters):
         if len(prototype.parameters) != len(input_method.parameters):
-            print("Prototype mismatch:", class_name, prototype, input_method)
-            if len(prototype.parameters) < len(input_method.parameters) and not (
-                len(prototype.parameters) == 1 and prototype.parameters[0].type == UNKNOWN
-            ):
-                print("\t Still updating info")
+            print(f"[Error] Parameters count mismatch on {class_name}: \n\t{prototype}\n\t{input_method}")
+            # If the method does not depend on the parameters, IDA might consider it a method without parameters.
+            # So I assume the one with the parameters is the correct one.
+            if not prototype.parameters:
                 prototype.parameters = input_method.parameters
             return
 
@@ -283,6 +336,11 @@ def enrich_prototype(class_name: str, prototype: MethodPrototype, input_method: 
                 proto_param.type = input_param.type
             if proto_param.name is None:
                 proto_param.name = proto_param.name
+
+
+def _has_unknown_parameters(parameters: list[MethodParam]) -> bool:
+    """Does this prototype have unknown parameters?"""
+    return len(parameters) == 1 and parameters[0].type == UNKNOWN
 
 
 if __name__ == "__main__":
